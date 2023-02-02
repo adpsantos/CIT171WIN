48e7bbc635f58775923a7bb2b439e2b8a4eff8ed
Angelica Santos <angelicadpsantos@Angelicas-MacBook-Air-3.local>
2023-02-02 12:22:04 -0700

    index.html

--- manageuser.js
+++ manageuser.js
@@ -60,7 +60,7 @@ function userlogin(){
     $.ajax({
         type: 'POST',
         url: 'https://dev.stedi.me/twofactorlogin',
-        data: JSON.stringify({phoneNumber:'2082706127', oneTimePassword:'8111---'}),
+        data: JSON.stringify({phoneNumber: userName , oneTimePassword: password}),
         success: function(data) {
             window.location.href = "/timer.html#"+data;//add the token to the url
         },
